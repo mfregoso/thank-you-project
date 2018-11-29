7995712905eda1824935916ebc6f1be5f0b5df43
mfregoso <mfregoso@users.noreply.github.com>
2018-11-28 23:24:54 -0800

    view story UX: "loading" and "not found"

--- react/src/components/ViewStory.js
+++ react/src/components/ViewStory.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { GetStoryById } from "../services/story.service";
 import moment from "moment";
+import logo from "../img/typ-logo.png";
 
 class ViewStory extends Component {
   state = {
@@ -8,7 +9,8 @@ class ViewStory extends Component {
     posterName: "",
     location: "",
     dayOfStory: "",
-    description: ""
+    description: "",
+    isLoading: true
   };
 
   componentDidMount() {
@@ -32,8 +34,11 @@ class ViewStory extends Component {
         location: story.location || "",
         description: story.description || "",
         latitude: story.latitude,
-        longitude: story.longitude
+        longitude: story.longitude,
+        isLoading: false
       });
+    } else {
+      this.setState({ isLoading: false });
     }
   };
 
@@ -48,32 +53,78 @@ class ViewStory extends Component {
   };
 
   render() {
+    const {
+      thankeeName,
+      description,
+      posterName,
+      isLoading,
+      dayOfStory,
+      location
+    } = this.state;
+
+    const showStory = () => {
+      if (!thankeeName && isLoading) {
+        return (
+            <h3 className="text-center">Loading...</h3>
+        );
+      } else if (!thankeeName) {
+        return (
+          <div className="mx-auto text-center">
+            <h3 className="font-weight-bold">
+              It looks like this story doesn't exist :/
+            </h3>
+            <br />
+            <h3>
+              Would you like to{" "}
+              <a
+                href="javascript:(0)"
+                onClick={() => this.props.history.push("/share")}
+              >
+                Share a Story
+              </a>
+              ?
+            </h3>
+            <img
+              src={logo}
+              alt="Thank You Project logo"
+              style={{ maxWidth: "55%", paddingTop: "2em" }}
+            />
+          </div>
+        );
+      } else if (thankeeName && !isLoading) {
+        return (
+          <React.Fragment>
+            <h3>Thank You, {thankeeName}</h3>
+            <div
+              className="marker-desc"
+              style={{
+                whiteSpace: "pre-wrap",
+                width: "100%"
+              }}
+            >
+              {description}
+            </div>
+            <div className="font-italic">
+              <strong>Sincerely,</strong>
+            </div>
+            <div>
+              <strong>- {posterName}</strong>
+            </div>
+            <br />
+            <div>
+              {/* Visual improvement needed here */}
+              This story happened on {dayOfStory} at {location}.
+            </div>
+            <br />
+          </React.Fragment>
+        );
+      }
+    };
+
     return (
       <div className="container-fluid view-spacer">
         <div className="mx-auto col-xl-6 col-lg-6 col-md-8 col-xs-12">
-          <h3>Thank You, {this.state.thankeeName}</h3>
-          <div
-            className="marker-desc"
-            style={{
-              whiteSpace: "pre-wrap",
-              width: "100%"
-            }}
-          >
-            {this.state.description}
-          </div>
-          <div className="font-italic">
-            <strong>Sincerely,</strong>
-          </div>
-          <div>
-            <strong>- {this.state.posterName}</strong>
-          </div>
-          <br />
-          <div>
-            {/* Visual improvement needed here */}
-            This story happened on {this.state.dayOfStory} at{" "}
-            {this.state.location}.
-          </div>
-          <br />
+          {showStory()}
         </div>
       </div>
     );
